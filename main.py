fb650a3eaf49ce14972cf9792b7cf21c727e74c7
cbeimers113 <chrisbeimers113@gmail.com>
2023-03-22 23:20:36 -0400

    Messaging upgrades

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ from discord.ext import tasks
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
-# Map username to {day: [cumulative, debt, flag]}
-# flag is whether this day's debt has been carried over to the next day
+# Map username to {day: [cumulative, debt, debt_check, warn]}
+# day: the record's date
+# cumulative: how many pushups the user has done on day
+# debt: how many pushups the user has missed on previous days, will accumulate first
+# debt_check: whether yesterday's debt has been carried forward for this user
+# warn: whether this user has been warned that time is running out for the day
 data: dict = {}
+msg_channel = discord.utils.get(client.get_all_channels(), name='General')
 
 if os.path.exists('data.pkl'):
     with open('data.pkl', 'rb') as f:
@@ -21,7 +26,8 @@ if os.path.exists('data.pkl'):
 async def on_ready() -> None:
     """Perform actions on startup."""
     await client.change_presence(activity=discord.Game(name='help! for usage'))
-    await debt_check.start()
+    await timed_tasks.start()
+    await msg_channel.send('💪')
 
 
 @client.event
@@ -90,13 +96,16 @@ async def on_message(message: discord.Message) -> None:
         await message.channel.send(f'{message.author.mention}\nYour pushups for {today}:\nCumulative: {data[user][today][0]}\nDebt: {data[user][today][1]}')
 
 
-@tasks.loop(minutes=1)
-async def debt_check() -> None:
-    """Run every day at midnight, carries over debt to the next day."""
-    today = str(date.today())
-    yesterday = str(date.today() - timedelta(days=1))
+@tasks.loop(minutes=0.5)
+async def timed_tasks() -> None:
+    """Run scheduled tasks."""
+    today: str = str(date.today())
+    yesterday: str = str(date.today() - timedelta(days=1))
+    hour: int = datetime.now().hour
+    minute: int = datetime.now().minute
 
-    if datetime.now().hour == 0:
+    # Carry over debt to the next day at midnight
+    if hour == 0 and minute == 0:
         for user in data:
             if today not in data[user]:
                 data[user][today] = [0, 0, False]
@@ -105,6 +114,19 @@ async def debt_check() -> None:
                 debt = 100 - data[user][yesterday][0]
                 data[user][today][1] = data[user][yesterday][1] + debt
                 data[user][yesterday][2] = True
+
+            print(data[user][yesterday])
+            print('↓')
+            print(data[user][today])
+            print(flush=True)
+
+    # Snap fingers at anyone who needs to do more pushups at 12:30
+    if hour == 23 and minute == 30:
+        warn_msg = '🫰🫰' + '\n'.join([
+            user.mention for user in msg_channel.members if data[user.name][today][0] < 100
+        ])
+
+        await msg_channel.send(warn_msg)
 
 with open('token.txt', 'r') as f:
     client.run(f.read())
